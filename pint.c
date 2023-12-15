25ae8165c7d36e71508e138906f519eb1f6b6e96
Relay90 <motaungr511@gmail.com>
2023-12-14 21:33:46 -0800

    monty project

--- pint.c
+++ pint.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * f_pint - Prints the value at the top of the stack without removing it
+ * @head: Double pointer to the top of the stack
+ * @counter: Line number in the Monty bytecode file
+ */
 void f_pint(stack_t **head, unsigned int counter)
 {
 	if (*head == NULL)
