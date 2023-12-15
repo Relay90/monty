9b6f1548e5e004960acdb578b39e0ad7ceddcd49
Relay90 <motaungr511@gmail.com>
2023-12-14 21:23:29 -0800

    monty project

--- push_pall.c
+++ push_pall.c
@@ -1,70 +1,59 @@
 #include "monty.h"
 
 /**
- * f_pall - print everything in stack
- * @head: pointer to pointer of first node
- * @counter: line counter
- *
- * Return: none
+ * f_push - Pushes an integer onto a stack or queue based on a condition.
+ * @head: Pointer to the head of the stack or queue.
+ * @counter: Line number or counter for error reporting.
+ */
+void f_push(stack_t **head, unsigned int counter)
+{
+	int i = 0, flag = 0;
+
+	if (!bus.arg || (bus.arg[0] == '-' && ++i) || !isdigit(bus.arg[i]))
+		flag = 1;
+	else
+	{
+		for (; bus.arg[i] != '\0'; i++)
+		{
+			if (!isdigit(bus.arg[i]))
+			{
+				flag = 1;
+				break;
+			}
+		}
+	}
+	if (flag)
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	i = atoi(bus.arg);
+	bus.lifi == 0 ? addnode(head, i) : addqueue(head, i);
+}
+
+/**
+ * f_pall - Prints the elements of a stack.
+ * @head: Pointer to the head of the stack.
+ * @counter: Line number or counter for identification.
  */
 void f_pall(stack_t **head, unsigned int counter)
 {
-	stack_t *h;
+	stack_t *h = *head;
 
-	h = *head;
 	printf("L%d: ", counter); /* Print the line number */
-	if (h == NULL)
+
+	if (!h)
 	{
 		printf("Stack is empty\n");
 		return;
 	}
-
-	while (h->next != NULL)
-	{
-		h = h->next;
-	}
-
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->prev;
+		h = h->next;
 	}
 }
 
-/**
- * f_push - function that adds node to the stack
- * @head: double head pointer to the stack
- * @counter: line count
- *
- * Return: nothing
- */
-void f_push(stack_t **head, unsigned int counter)
-{
-	int i, m = 0, flag = 0;
-
-	if (bus.arg)
-	{
-		if (bus.arg[0] == '-')
-			m++;
-		for (; bus.arg[m] != '\0'; m++)
-		{
-			if (bus.arg[m] > 57 || bus.arg[m] < 48)
-				flag = 1; }
-		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-			fclose(bus.file);
-			free(bus.content);
-			free_stack(*head);
-			exit(EXIT_FAILURE); }}
-	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
-		fclose(bus.file);
-		free(bus.content);
-		free_stack(*head);
-		exit(EXIT_FAILURE); }
-	i = atoi(bus.arg);
-	if (bus.lifi == 0)
-		addnode(head, i);
-	else
-		addqueue(head, i);
-}
